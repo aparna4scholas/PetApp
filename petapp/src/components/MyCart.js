bb9909e45b097c3c5f66d02b081491a01d438e0a
aparna4scholas <aparna4scholas@gmail.com>
2020-06-02 07:40:26 -0500

    modified

--- petapp/src/components/MyCart.js
+++ petapp/src/components/MyCart.js
@@ -28,12 +28,18 @@ class MyCart extends Component {
         localStorage.removeItem("data");
 
     }
+    checkout(){
+        alert('checkout');
+      
+
+    }
 
 
     render() {
         return (
             <div>
                 <button onClick={this.clear}> clear cart</button>
+                <button onClick={this.checkout}> checkout</button>
                 <div className="sss">
                 {this.state.cart.map((breed) => (
                     <div >
@@ -50,48 +56,3 @@ class MyCart extends Component {
     }
 }
 export default MyCart;
-{/*
-import Books from './Books.js'
-import React, { Component } from 'react';
-
-class Savebooks extends Component {
-    state = { 
-        favBooks : []
-    }
-
-    componentWillMount(){
-        //alert(localStorage.getItem('data'));
-       // this.state.favBooks = JSON.parse(localStorage.getItem('data'));
-        localStorage.getItem('data') && this.setState({
-            favBooks: JSON.parse(localStorage.getItem('data')),
-            isLoading : false
-        })
-        alert(this.state.favBooks.length);
-        //this.getData();
-    }
-render() {
-    return (
-        <div className="div_class">
-           <h2> My Favourite Books </h2>  
-            {this.state.favBooks.map((book) => (
-                <div className="results-container">
-                    <table class="table table-dark">
-                        <thead>
-                            <th scope="col">Book</th>
-                            <th scope="col">Title</th>
-                        </thead>
-                        <tr scope="row">
-                        <td>{<img id={book._id} src={book.url} alt="No value" />}</td>
-                        <td class="text-white-50 align-text-bottom" >{book.title}
-                        </td>
-                        </tr>
-                    </table>
-                </div>
-            ))}
-        </div>
-
-)
-}
-}
-
-export default Savebooks;*/}
