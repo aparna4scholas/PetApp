3ded910029031da6d39614b14dab17b2c27daf90
aparna4scholas <aparna4scholas@gmail.com>
2020-06-02 10:55:25 -0500

    modified

--- petapp/src/components/Home.js
+++ petapp/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
-
+import img1 from '../components/assets/img1.png';
+ 
  class Home extends Component {
     render() {
         return (
             <div>
-                <h1> Home</h1>
+                
+                <div className="img_1">
+            <img className="img1" src={img1} alt='mypet' />
+          </div>
             </div>
         )
     }
